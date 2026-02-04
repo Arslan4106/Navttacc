887e97549a2950edec1ce6b08ed2f824bfd2f358
Muhammad Arslan <116803961+Arslan4106@users.noreply.github.com>
2026-02-04 10:10:34 +0500

    Add files via upload

---
+++ Lecture12/script.js
@@ -0,0 +1,18 @@
+// Values and Variables
+
+// Variables: Memory Containers
+// x = 23;
+// y = 44;
+// z = 22;
+// v = x + y + z;
+// firstName = "Jonas";
+let x = 45;
+let y = 34;
+let z = x + y;
+console.log(z);
+console.log("x + y = ", z);
+
+let firstName = "Jonas";
+let lastName = "Bob";
+console.log(firstName, lastName);
+console.log("firstName");
---
+++ Lecture13/script.js
@@ -0,0 +1,72 @@
+// Values and Variables
+// Rules to Naming the Variables
+// => You cannot use number at starting
+// e.g let 12firstName
+// let firstName12;
+// => You cannot use Space between variable name;
+// e.g let first Name;
+// let first Name;
+// => You cannot use symbol in Variable Name
+// e.g let first&Name;
+// let first@Name;
+// => You cannot use - in variable Name
+// e.g let first-Name;
+// let first-Name;
+// => You cannot Use Builtin words(reserved Word, Keywords) in variable Name,
+// e.g let console;
+// let console.log;
+// let function;
+
+// Variable Declaration
+let x;
+// Variable Initilization
+let a = 56.8;
+let y = 45;
+let d = a > y;
+console.log(d);
+let z = a + y;
+console.log(z);
+console.log("firstName");
+let firstName = "Imran";
+let lastName = "Khan";
+console.log(firstName, lastName);
+console.log("I am a MErn STACK  Developer");
+
+// Data:
+// => Primptive Data:
+// There seven types of Pimptive Data
+// => 1. Number (Numbers or Intigers)
+let num1 = 23.7;
+let num2 = 44.0;
+console.log(num1, num2);
+console.log(typeof num1, typeof num2);
+// => 2. String (Chracters)
+let myfirstName = "Imran";
+let mylastName = "Khan";
+console.log(myfirstName, mylastName);
+console.log(typeof myfirstName, typeof mylastName);
+// => 3. Boolean (true or false)
+let g = 34;
+let h = 67;
+let k = g == h;
+console.log(k);
+console.log(typeof k);
+
+// => 4. Undefined (empty Value)
+let newnum;
+console.log(newnum);
+console.log(typeof newnum);
+
+// => 5. Null (Empty Value)
+let l = 0;
+console.log(typeof l);
+// => 6. Symbol (Unique type of Data)
+
+// => 7. BigInt (Large Data or huge Data)
+// let n = 1223456788..........N;
+
+// => Typeof Operator
+// => Non-Primptive
+// Arrays and Objects
+let student = [];
+console.log(typeof student);
